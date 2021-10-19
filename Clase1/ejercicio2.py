f6b7f6fe82f1b8d7c91b98ce428231d41f572f45
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-10-19 20:09:39 -0300

    fix Keys import

--- Clase1/ejercicio2.py
+++ Clase1/ejercicio2.py
@@ -1,6 +1,5 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
